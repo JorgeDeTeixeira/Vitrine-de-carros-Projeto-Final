03235e997090b3e0b16b12af6409c582d88075a5
Jorge de Teixeira <jorgedeteixeirademelo@gmail.com>
2022-12-24 14:54:36 -0300

    Listando as marcas

--- marca/src/main/java/vitrine/marca/controllers/MarcaController.java
+++ marca/src/main/java/vitrine/marca/controllers/MarcaController.java
@@ -1,14 +1,19 @@
 package vitrine.marca.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import vitrine.marca.models.Marca;
 import vitrine.marca.repositories.MarcaRepository;
 
 @Controller
+@RequestMapping("/vitrine")
 public class MarcaController {
 
 	@Autowired
@@ -18,17 +23,25 @@ public class MarcaController {
 	public String home() {
 		return "home";
 	}
-	
-	@RequestMapping("/vitrine/marca/form")
+
+	@GetMapping("/marca/form")
 	public String formulario() {
 		return "marca/formMarca";
 	}
 
-	@PostMapping("/vitrine/marca")
+	@PostMapping("marca")
 	public String adicionarMarca(Marca marca) {
 		System.out.println(marca);
 		marcaRepository.save(marca);
 
 		return "marca/marcaAdicionada";
+	}
+
+	@GetMapping("marcas")
+	public ModelAndView listarMarcas() {
+		List<Marca> marcas = marcaRepository.findAll();
+		ModelAndView mv = new ModelAndView("marca/lista");
+		mv.addObject("marcas", marcas);
+		return mv;
 	}
 }
