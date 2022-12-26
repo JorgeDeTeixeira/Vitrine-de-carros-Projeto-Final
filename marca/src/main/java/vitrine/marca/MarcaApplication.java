b2821009b0702bc769d07f06f65d714ce2088d30
PedroSouza123IFRN <Emanoelpedro042@gmail.com>
2022-12-26 09:14:51 -0300

    Auth em base de dados via JPA

--- marca/src/main/java/vitrine/marca/MarcaApplication.java
+++ marca/src/main/java/vitrine/marca/MarcaApplication.java
@@ -2,12 +2,14 @@ package vitrine.marca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MarcaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarcaApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 }
